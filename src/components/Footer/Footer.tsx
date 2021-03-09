a45736eeabf2b413f12c36c08c055827aba48117
Haiying Weng <hw534@cornell.edu>
2021-03-08 20:38:23 -0500

    Address PR comments

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ class Footer extends React.Component {
       <div className="footer">
         <div className="container">
           <p>
-            Originally created by Ning Ning Sun and Chase Thomas in 2016. Currently maintained by <a href="https://cornellappdev.com">Cornell AppDev.</a>
+            Originally created by Ning Ning Sun and Chase Thomas in 2016. Currently maintained by <a href="https://cornellappdev.com">Cornell AppDev</a>.
           </p>
         </div>
       </div>
