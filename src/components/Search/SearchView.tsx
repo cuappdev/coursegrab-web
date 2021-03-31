968fc19fab1aab00e0190d686fb5a424e49d97e2
Haiying Weng <hw534@cornell.edu>
2021-03-31 19:16:45 -0400

    Make request to search endpoint

--- src/components/Search/SearchView.tsx
+++ src/components/Search/SearchView.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import './SearchView.css'
 
+import {
+  searchCourses
+} from '../../utils/requests';
+
 class SearchView extends React.Component {
   state = {
     searchText: '',
@@ -17,10 +21,17 @@ class SearchView extends React.Component {
   searchIcon = (width: number, height: number) => {
     return (
       <svg className="search-icon" width={width} height={height} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="6.5" cy="6.5" r="5.5" stroke="#9AA0A6" stroke-width="2" />
+        <circle cx="6.5" cy="6.5" r="5.5" stroke="#9AA0A6" strokeWidth="2" />
         <rect x="9.74316" y="11.1411" width="2" height="8.75923" rx="1" transform="rotate(-44.5128 9.74316 11.1411)" fill="#9AA0A6" />
       </svg>
     )
+  }
+
+  getCourses = async (query: string) => {
+    if (this.state.searchText.length > 2) {
+      await searchCourses(query)
+    }
+    // console.log(searchCourses(query))
   }
 
   render() {
@@ -38,7 +49,11 @@ class SearchView extends React.Component {
           {this.searchIcon(18, 18)}
           <input className="search-input"
             value={this.state.searchText}
-            onChange={(event) => this.setState({ searchText: event.target.value })} />
+            onChange={(event) => {
+              this.setState({ searchText: event.target.value })
+              this.getCourses(this.state.searchText)
+            }
+            } />
         </div>
         {
           this.state.searchText === ''
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -14,7 +14,7 @@ const get = async (url: string, headers?: any, params?: any) => {
     if (success) return data
     throw Error(data)
 }
-  
+
 const post = async (url: string, headers?: any, body?: any) => {
     const res = await api.post(url, headers, body)
     const { success, data } = res.data
@@ -23,22 +23,25 @@ const post = async (url: string, headers?: any, body?: any) => {
 }
 
 export const setAuthHeader = (token: string | null) => {
-  const header = `Bearer ${token ? token : localStorage.getItem('accessToken')}`
-  axios.defaults.headers.common['Authorization'] = header
+    const header = `Bearer ${token ? token : localStorage.getItem('accessToken')}`
+    axios.defaults.headers.common['Authorization'] = header
 }
 
-export const validateToken = async () => {}
+export const validateToken = async () => { }
 
-export const initializeSession = async () => {}
+export const initializeSession = async () => { }
 
-export const updateSession = async () => {}
+export const updateSession = async () => { }
 
-export const getAllTrackedSections = async () => {}
+export const getAllTrackedSections = async () => { }
 
-export const searchCourse = async () => {}
+export const searchCourses = async (query: string) => {
+    const data = await post(`/courses/search/`, {}, { query: query });
+    return data;
+}
 
-export const trackSection = async () => {}
+export const trackSection = async () => { }
 
-export const untrackSection = async () => {}
+export const untrackSection = async () => { }
 
-export const setNotificationMode = async () => {}
+export const setNotificationMode = async () => { }
