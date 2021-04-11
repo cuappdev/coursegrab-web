ca498783df71024eb5bb8496e87b2712fca0de8c
Haiying Weng <48968041+haiyingweng@users.noreply.github.com>
2021-04-11 19:09:31 -0400

    Implement currently tracked courses UI (#8)
    * Make request to search endpoint
    
    * Add preliminary elements
    
    * Add more dynamic info to TrackedCourseCard
    
    * Add styles for TrackedCourseCard
    
    * Align tracked course card header
    
    * Address PR comments

---
+++ src/components/TrackedCourses/TrackedCourseCard.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import './TrackedCourseCard.css'
+
+import { Section, Status } from '../../types'
+
+export interface TrackedCourseCardProps {
+  section: Section
+}
+
+const TrackedCourseCard: React.FunctionComponent<TrackedCourseCardProps> = ({
+  section
+}) => {
+  const statusIcon = (status: Status) => {
+    switch (status) {
+      case Status.OPEN:
+        return (
+          <svg className="status-icon" height="16" width="16">
+            <circle cx="8" cy="8" r="8" fill="#47C753" />
+          </svg>
+        )
+      case Status.CLOSED:
+        return (
+          <svg className="status-icon" height="16" width="16">
+            <rect width="16" height="16" fill="#CA4238" />
+          </svg>
+        )
+      case Status.WAITLISTED:
+        return (
+          <svg className="status-icon" height="16" width="16">
+            <polygon points="0,16 8,0 16,16" fill="#FFD027" />
+          </svg>
+        )
+    }
+  }
+  return (
+    <div className="tracked-course-card" >
+      <div className="title-status-view">
+        <p className="title-label">{section.title}</p>
+        {statusIcon(section.status)}
+      </div>
+      <div className="course-info-view">
+        <p className="section-label">{section.section}</p>
+        <p className="catalog-num-label">{section.catalogNum}</p>
+        <p className="mode-label">{section.mode}</p>
+      </div>
+      <div className="action-buttons">
+        <button className="remove-button" >REMOVE</button>
+        {
+          section.status === Status.OPEN
+            ? <button className="enroll-button">ENROLL</button>
+            : null
+        }
+      </div>
+    </div >
+  )
+}
+
+export default TrackedCourseCard
--- src/components/TrackedCourses/TrackedCoursesView.tsx
+++ src/components/TrackedCourses/TrackedCoursesView.tsx
@@ -2,15 +2,90 @@ import React from 'react'
 
 import './TrackedCoursesView.css'
 
+import { Section, Status } from '../../types'
+
+import TrackedCourseCard from './TrackedCourseCard'
+
 class TrackedCoursesView extends React.Component {
+  state = {
+    availableSections: [
+      {
+        catalogNum: 1111,
+        courseNum: 1000,
+        instructors: [],
+        isTracking: true,
+        mode: 'Asynchronous',
+        numTracking: 5,
+        section: 'LEC 001 / TR 12:20pm - 1:10pm',
+        status: Status.OPEN,
+        subjectCode: 'INFO',
+        title: 'INFO 1300: Introduction to Web Design'
+      }
+    ],
+    awaitingSections: [
+      {
+        catalogNum: 1000,
+        courseNum: 1000,
+        instructors: [],
+        isTracking: true,
+        mode: 'Asynchronous',
+        numTracking: 5,
+        section: 'LEC 001 / TR 12:20pm - 1:10pm',
+        status: Status.WAITLISTED,
+        subjectCode: 'INFO',
+        title: 'INFO 1300: Introduction to Web Design'
+      },
+      {
+        catalogNum: 1234,
+        courseNum: 1000,
+        instructors: [],
+        isTracking: true,
+        mode: 'Asynchronous',
+        numTracking: 5,
+        section: 'LEC 001 / TR 12:20pm - 1:10pm',
+        status: Status.CLOSED,
+        subjectCode: 'INFO',
+        title: 'INFO 1300: Introduction to Web Design'
+      }
+    ]
+  }
+
   render() {
+    const availableSectionsView = this.state.availableSections.map(section => {
+      return (
+        <TrackedCourseCard section={section} />
+      )
+    })
+    const awaitingSections = this.state.awaitingSections.map(section => {
+      return (
+        <TrackedCourseCard section={section} />
+      )
+    })
+    const sections = () => {
+      if (this.state.availableSections.length > 0 && this.state.awaitingSections.length > 0) {
+        return (
+          <div>
+            <p className="num-available-label">{`${this.state.availableSections.length} Available`}</p>
+            {availableSectionsView}
+            <p className="num-awaiting-label">{`${this.state.awaitingSections.length} Awaiting`}</p>
+            {awaitingSections}
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <svg height="70" width="70">
+              <circle cx="35" cy="35" r="35" fill="#47C753" />
+            </svg>
+            <p className="tracking-title-label">No Courses Currently Tracked</p>
+            <p className="tracking-subtitle-label">Tap the search icon to start adding courses</p>
+          </div>
+        )
+      }
+    }
     return (
       <div className="tracked-courses-view">
-        <svg height="70" width="70">
-          <circle cx="35" cy="35" r="35" fill="#47C753" />
-        </svg>
-        <p className="tracking-title-label">No Courses Currently Tracked</p>
-        <p className="tracking-subtitle-label">Tap the search icon to start adding courses</p>
+        {sections()}
       </div>
     )
   }
