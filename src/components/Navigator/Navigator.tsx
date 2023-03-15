2e3bd9e4bc4a23d5a59ba455a911c41bfcd1ccc8
Noah Solomon <njs99@cornell.edu>
2023-03-15 00:12:30 -0400

    Remove commented code + console.logs

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -11,44 +11,6 @@ import { ReactComponent as CourseGrabLogo } from '../../Bell.svg';
 import { initializeSession } from '../../utils/requests';
 import { User, SessionAuthorization } from '../../types'
 
-// const googleAuth = async () => {
-
-//   await signInWithPopup(auth, new GoogleAuthProvider()).then((res) => {
-//     const credential = GoogleAuthProvider.credentialFromResult(res);
-//     const token = credential?.idToken;
-//     // const accessToken = credential?.accessToken;
-//     const user = res.user;
-//     const displayName = (user.displayName || '').split(' ');
-//     let givenName, familyName;
-//     if (displayName.length > 1) {
-//       givenName = displayName[0];
-//       familyName = displayName[1];
-//     } else {
-//       givenName = displayName[0];
-//       familyName = '';
-//     }
-//     // const profile = user.profile as Record<string, string>
-//     const session = await initializeSession(token!, givenName, familyName) as SessionAuthorization
-//     const loggedInUser: User = {
-//       email: user.email || '',
-//       name: user.displayName || '',
-//       id: token || '',
-//       sessionAuthorization: session,
-//     }
-//     localStorage.setItem('user', JSON.stringify(loggedInUser));
-//     localStorage.setItem('googleToken', token);
-//     console.log(token);
-//     this.setState({ isSignedIn: true });
-
-//     window.location.reload();
-//   }).catch(error => {
-//     console.log(error);
-//   })
-
-// }
-
-// var provider = new firebase.auth.GoogleAuthProvider()
-
 class Navigator extends React.Component {
 
   state = {
@@ -60,7 +22,6 @@ class Navigator extends React.Component {
       .then(async result => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.idToken as string;
-        // const user = result.additionalUserInfo as firebase.auth.AdditionalUserInfo
         const user = result.user;
         const displayName = (user.displayName || '').split(' ');
         let givenName, familyName;
@@ -71,7 +32,6 @@ class Navigator extends React.Component {
           givenName = displayName[0];
           familyName = '';
         }
-        console.log("1: got token");
         const session = await initializeSession(token, givenName, familyName) as SessionAuthorization
         const loggedInUser: User = {
           email: user.email || '',
@@ -79,12 +39,10 @@ class Navigator extends React.Component {
           id: token,
           sessionAuthorization: session,
         }
-        console.log("2: got user");
         localStorage.setItem('user', JSON.stringify(loggedInUser));
         localStorage.setItem('googleToken', token);
         this.setState({ isSignedIn: true })
         // refresh page so other components are updated
-        console.log("3: about to reload ");
         window.location.reload()
       })
       .catch(error => {
