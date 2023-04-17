f0f6d3ba431d8b0a029c8f5a82d75c1dd0004bad
Noah Solomon <njs99@cornell.edu>
2023-04-17 15:25:04 -0400

    Update emojis

--- src/components/DetailCourse/DetailCourseCardCell.tsx
+++ src/components/DetailCourse/DetailCourseCardCell.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import './DetailCourseCardCell.css'
+import "./DetailCourseCardCell.css";
 
-import { trackSection, untrackSection } from '../../utils/requests';
-import { Section, Status } from '../../types'
+import { trackSection, untrackSection } from "../../utils/requests";
+import { Section, Status } from "../../types";
 
 export interface DetailSectionCellProps {
-  section: Section
+  section: Section;
 }
 
 const DetailSection: React.FunctionComponent<DetailSectionCellProps> = ({
-  section
+  section,
 }) => {
   const [isTracking, setIsTracking] = useState(section.isTracking);
 
@@ -21,55 +21,59 @@ const DetailSection: React.FunctionComponent<DetailSectionCellProps> = ({
           <svg height="16" width="16">
             <circle cx="8" cy="8" r="8" fill="#47C753" />
           </svg>
-        )
+        );
       case Status.CLOSED:
         return (
           <svg height="16" width="16">
             <rect width="16" height="16" fill="#CA4238" />
           </svg>
-        )
+        );
       case Status.WAITLISTED:
         return (
           <svg height="16" width="16">
             <polygon points="0,16 8,0 16,16" fill="#FFD027" />
           </svg>
-        )
+        );
     }
-  }
+  };
 
   const trackClicked = async () => {
     try {
-      const result = await trackSection(section.catalogNum)
-      if (result.catalogNum == section.catalogNum) setIsTracking(true)
+      const result = await trackSection(section.catalogNum);
+      if (result.catalogNum === section.catalogNum) setIsTracking(true);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const removeClicked = async () => {
     try {
-      const result = await untrackSection(section.catalogNum)
-      if (result.catalogNum == section.catalogNum) setIsTracking(false)
+      const result = await untrackSection(section.catalogNum);
+      if (result.catalogNum === section.catalogNum) setIsTracking(false);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
-    <div className="detail-section" >
+    <div className="detail-section">
       {statusIcon(section.status)}
       <p className="description-label">{section.section}</p>
-      {
-        isTracking
-          ? <button className="remove-button" onClick={removeClicked}>REMOVE</button>
-          : <button className="track-button" onClick={trackClicked}>TRACK</button>
-      }
-      <p className="emoji-icon">🙋🏾‍♀️</p>
+      {isTracking ? (
+        <button className="remove-button" onClick={removeClicked}>
+          REMOVE
+        </button>
+      ) : (
+        <button className="track-button" onClick={trackClicked}>
+          TRACK
+        </button>
+      )}
+      <p className="emoji-icon">👥</p>
       <p className="num-tracking-label">{`${section.numTracking} students are tracking this section`}</p>
-      <p className="emoji-icon">👀</p>
+      <p className="emoji-icon">📍</p>
       <p className="detail-cell-mode-label">{section.mode}</p>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default DetailSection
+export default DetailSection;
--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,79 +1,92 @@
-import React from 'react'
+import React from "react";
 
-import './Navigator.css'
+import "./Navigator.css";
 // import firebase from "firebase/app"
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from '../../firebase/init';
+import { auth } from "../../firebase/init";
 
-import SearchView from '../Search/SearchView'
+import SearchView from "../Search/SearchView";
 
-import { ReactComponent as CourseGrabLogo } from '../../Bell.svg';
-import { initializeSession } from '../../utils/requests';
-import { User, SessionAuthorization } from '../../types'
+import { ReactComponent as CourseGrabLogo } from "../../Bell.svg";
+import { initializeSession } from "../../utils/requests";
+import { User, SessionAuthorization } from "../../types";
 
 class Navigator extends React.Component {
-
   state = {
-    isSignedIn: !!localStorage.getItem('user')
-  }
+    isSignedIn: !!localStorage.getItem("user"),
+  };
 
   openSigninPopup = () => {
     signInWithPopup(auth, new GoogleAuthProvider())
-      .then(async result => {
+      .then(async (result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.idToken as string;
         const user = result.user;
-        const displayName = (user.displayName || '').split(' ');
+        const displayName = (user.displayName || "").split(" ");
         let givenName, familyName;
         if (displayName.length > 1) {
           givenName = displayName[0];
           familyName = displayName[1];
         } else {
           givenName = displayName[0];
-          familyName = '';
+          familyName = "";
         }
-        const session = await initializeSession(token, givenName, familyName) as SessionAuthorization
+        const session = (await initializeSession(
+          token,
+          givenName,
+          familyName
+        )) as SessionAuthorization;
         const loggedInUser: User = {
-          email: user.email || '',
-          name: user.displayName || '',
+          email: user.email || "",
+          name: user.displayName || "",
           id: token,
           sessionAuthorization: session,
-        }
-        localStorage.setItem('user', JSON.stringify(loggedInUser));
-        localStorage.setItem('googleToken', token);
-        this.setState({ isSignedIn: true })
+        };
+        localStorage.setItem("user", JSON.stringify(loggedInUser));
+        localStorage.setItem("googleToken", token);
+        this.setState({ isSignedIn: true });
         // refresh page so other components are updated
-        window.location.reload()
+        window.location.reload();
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      })
-  }
+      });
+  };
 
   signOut = () => {
-    auth.signOut()
-    localStorage.removeItem('user')
-    localStorage.removeItem('googleToken')
-    this.setState({ isSignedIn: false })
+    auth.signOut();
+    localStorage.removeItem("user");
+    localStorage.removeItem("googleToken");
+    this.setState({ isSignedIn: false });
     // refresh page so other components are updated
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   render() {
     return (
       <div className="nav-view">
         <div className="container">
-          <div className="nav-left" >
+          <div className="nav-left">
             <CourseGrabLogo />
-            <a className="coursegrab-text" href="/">CourseGrab</a>
+            <a className="coursegrab-text" href="/">
+              CourseGrab
+            </a>
             <SearchView />
           </div>
           <div className="nav-right">
-            <button className="sign-in-out-button" onClick={() => this.state.isSignedIn ? this.signOut() : this.openSigninPopup()}> {this.state.isSignedIn ? "Sign Out" : "Sign In"}</button>
+            <button
+              className="sign-in-out-button"
+              onClick={() =>
+                this.state.isSignedIn ? this.signOut() : this.openSigninPopup()
+              }
+            >
+              {" "}
+              {this.state.isSignedIn ? "Sign Out" : "Sign In"}
+            </button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
